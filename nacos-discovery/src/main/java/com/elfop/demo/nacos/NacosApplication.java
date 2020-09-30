0165d4f16fbab6c2d024447250c2e9441b867168
liu zhenming <1119264845@qq.com>
2020-09-30 17:37:04 +0800

    init : 初始化项目 (nacos服务发现组件)

---
+++ nacos-discovery/src/main/java/com/elfop/demo/nacos/NacosApplication.java
@@ -0,0 +1,27 @@
+package com.elfop.demo.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+    @RestController
+    public static class EchoController {
+
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "1 Hello Nacos Discovery " + string;
+        }
+    }
+
+}
